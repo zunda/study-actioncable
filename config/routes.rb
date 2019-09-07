871a8773bb6a5ba4f88f22ab765c9b9d66b29cfd
zunda <zundan@gmail.com>
2019-09-06 15:03:16 -1000

    Fix root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,3 @@
 Rails.application.routes.draw do
-  get 'rooms/show'
-  root to: 'rooms/show'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'rooms#show'
 end
