d579bc02ab77623926d5d5424465a5d210e50db6
zunda <zundan@gmail.com>
2019-09-20 17:19:26 -1000

    Make ping interval configurable through env vars
    Default values seem to be:
    - Ping from app to client: `AC_BEAT_INTERVAL=5`
    - Interval to try reconnecting from client: `AC_STALE_THRESHOLD=10`

--- config/initializers/action_cable.rb
+++ config/initializers/action_cable.rb
@@ -2,7 +2,7 @@ if ENV['AC_BEAT_INTERVAL']
   module ActionCable
     module Server
       module Connections
-        BEAT_INTERVAL = Integer(ENV['AC_BEAT_INTERVAL'])
+        BEAT_INTERVAL = ENV['AC_BEAT_INTERVAL'].to_f
       end
     end
   end
