17efca6a5b1e62fd6fe295146fd326e47257bb9f
zunda <zundan@gmail.com>
2019-11-21 19:36:38 -1000

    Add a route for test

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root to: 'rooms#show'
+  get '/rooms/show', to: 'rooms#show'
 
   # Serve websocket cable requests in-process
   mount ActionCable.server => '/cable'
